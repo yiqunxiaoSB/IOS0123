bb4f062fa499b040df984879b5a9f1c95b008fa3
649697871zhang <649697871@qq.com>
2016-06-24 17:54:42 +0800

    zhg
    123

---
+++ zhanghaigang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhanghaigang
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 ZhangHaiGang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhanghaigang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhanghaigang
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 ZhangHaiGang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
